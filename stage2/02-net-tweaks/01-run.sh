f05ff335755a651ee779d782308c494b3fdf28fd
oxmc <67136658+oxmc@users.noreply.github.com>
2023-05-03 12:20:04 -0700

    Update 01-run.sh

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -1,40 +1,54 @@
 #!/bin/bash -e
 
-install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
-install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
+nm=dhcp
 
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 1
+nm_dhcp {
+  install -v -d "${ROOTFS_DIR}/etc/wpa_supplicant"
+  install -v -m 600 files/wpa_supplicant.conf "${ROOTFS_DIR}/etc/wpa_supplicant/"
+  
+  on_chroot << EOF
+    SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
+    SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 1
 EOF
-
-if [ -v WPA_COUNTRY ]; then
-	on_chroot <<- EOF
-		SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_country "${WPA_COUNTRY}"
-	EOF
-fi
-
-if [ -v WPA_ESSID ] && [ -v WPA_PASSWORD ]; then
-on_chroot <<EOF
-set -o pipefail
-wpa_passphrase "${WPA_ESSID}" "${WPA_PASSWORD}" | tee -a "/etc/wpa_supplicant/wpa_supplicant.conf"
+  
+  if [ -v WPA_COUNTRY ]; then
+    on_chroot <<- EOF
+      SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_country "${WPA_COUNTRY}"
+    EOF
+  fi
+  
+  if [ -v WPA_ESSID ] && [ -v WPA_PASSWORD ]; then
+    on_chroot << EOF
+      set -o pipefail
+      wpa_passphrase "${WPA_ESSID}" "${WPA_PASSWORD}" | tee -a "/etc/wpa_supplicant/wpa_supplicant.conf"
 EOF
-elif [ -v WPA_ESSID ]; then
-cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf" << EOL
-
-network={
-	ssid="${WPA_ESSID}"
-	key_mgmt=NONE
-}
+  elif [ -v WPA_ESSID ]; then
+    cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf" << EOL
+      network={
+        ssid="${WPA_ESSID}"
+        key_mgmt=NONE
+      }
 EOL
-fi
-
-# Disable wifi on 5GHz models if WPA_COUNTRY is not set
-mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
-if [ -n "$WPA_COUNTRY" ]; then
+  fi
+  
+  # Disable wifi on 5GHz models if WPA_COUNTRY is not set
+  mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
+  if [ -n "$WPA_COUNTRY" ]; then
     echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
     echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
-else
+  else
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
+  fi
+}
+
+if [ "$nm" == "dhcp" ]; then
+  nm_dhcp()
+elif [ "$nm" = "nm" ]; then
+  on_chroot << EOF
+    SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
+    SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 2
+EOF
+else
+  nm_dhcp()
 fi
