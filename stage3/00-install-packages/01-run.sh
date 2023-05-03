1d1289650974f3b102288aca6b4ee9e1a332fd2c
oxmc <67136658+oxmc@users.noreply.github.com>
2023-05-03 09:49:43 -0700

    Update 01-run.sh

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -6,4 +6,7 @@ update-alternatives --install /usr/bin/x-www-browser \
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
   apt-mark auto python3-pyqt5 python3-opengl
+  touch /etc/polkit-1/localauthority.conf.d/51-admin.conf
+  sh -c 'echo "[Configuration]" >> /etc/polkit-1/localauthority.conf.d/51-admin.conf'
+  sh -c 'echo "AdminIdentities=unix-group:sudo;unix-group:admin" >> /etc/polkit-1/localauthority.conf.d/51-admin.conf'
 EOF
