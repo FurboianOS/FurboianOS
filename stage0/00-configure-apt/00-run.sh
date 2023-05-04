848ed89adcf18bf792f4b8b3e9240959eff6a4cb
oxmc <67136658+oxmc@users.noreply.github.com>
2023-05-04 13:00:51 -0700

    Update 00-run.sh

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -15,6 +15,9 @@ fi
 cat files/raspberrypi.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg"
 install -m 644 "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
 on_chroot << EOF
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
 apt-get update
 apt-get dist-upgrade -y
 EOF
