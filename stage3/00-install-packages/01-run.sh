6801b91fb3e72574425af8941b4227d47997966e
oxmc <67136658+oxmc@users.noreply.github.com>
2023-07-25 03:23:19 -0700

    Add gdm on startup

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -7,9 +7,11 @@ touch /etc/polkit-1/localauthority.conf.d/51-admin.conf
   sh -c 'echo "[Configuration]" >> /etc/polkit-1/localauthority.conf.d/51-admin.conf'
   sh -c 'echo "AdminIdentities=unix-group:sudo;unix-group:admin" >> /etc/polkit-1/localauthority.conf.d/51-admin.conf'
   systemctl set-default graphical.target
-update-alternatives --install /usr/bin/x-www-browser \
+  systemctl start gdm
+  systemctl enable gdm
+  update-alternatives --install /usr/bin/x-www-browser \
   x-www-browser /usr/bin/chromium-browser 86
-update-alternatives --install /usr/bin/gnome-www-browser \
+  update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
   apt-mark auto python3-pyqt5 python3-opengl
   if [ "$remove_games" == "y" ]; then
